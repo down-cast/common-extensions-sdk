8743f11c8627be53304a79ace214a9fc119497e0
Mihail Brinza <mihail.brinza96@hotmail.com>
2022-06-30 22:07:02 +0100

    add initial configuration version of serilog

---
+++ src/Downcast.Common.Logging/LogExtensions.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using ILogger = Serilog.ILogger;
+
+namespace Downcast.Common.Logging;
+
+public static class LogExtensions
+{
+    public static void ConfigureSerilog(this WebApplicationBuilder builder)
+    {
+        builder.Configuration.AddJsonFile("serilog-settings.json", false, false);
+        builder.Logging.ClearProviders();
+        ILogger logger = Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+
+        builder.Host.UseSerilog(logger);
+    }
+}
